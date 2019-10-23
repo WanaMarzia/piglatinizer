5d00d079b755d50f1e838467feea5f2236a40894
Mika <mikaduckling@gmail.com>
2019-10-23 22:27:02 +0000

    added the click handler

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$(#botton).click(function() {
+let message;
+$(“output”);
+}
 
 
 
-  
-  
-  
-  
+
+
+
 });
